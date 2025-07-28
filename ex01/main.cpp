354202842120201eb68f9a1bceb320698541d7ea
funa <orz.home35@gmail.com>
2025-07-28 13:03:44 +0900

    📝 テストケースを少し修正

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,23 +1,25 @@
 #include"iter.hpp"
 
-void toUpperPrint(char& value) {
+void non_const_ToUpper(char& value) {
 	value = static_cast<char>(std::toupper(value));
 	std::cout << value << std::endl;
 }
 
-void Print(const char& value) {
+void const_ToUpper(const char& value) {
 	std::cout << value << std::endl;
 }
 
 int main( void )
 {
-	char a[] = {'a', 'b', 'c'};
-	size_t a_len = 3;
-
-	::iter( a, a_len, toUpperPrint);
-
 	char b[] = {'a', 'b', 'c'};
 	size_t b_len = 3;
-	::iter( b, b_len, Print);
+	::iter( b, b_len, const_ToUpper);
+
+	std::cout << std::endl;
+
+	char a[] = {'a', 'b', 'c'};
+	size_t a_len = 3;
+	::iter( a, a_len, non_const_ToUpper);
+
 	return 0;
 }
